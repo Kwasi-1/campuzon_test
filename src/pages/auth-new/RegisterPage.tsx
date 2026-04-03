95428e1cf7dff71a7a3170833448105aafd274ea
kwasi <affulnana04@gmail.com>
2026-04-03 05:35:37 +0000

    update email

--- src/pages/auth-new/RegisterPage.tsx
+++ src/pages/auth-new/RegisterPage.tsx
@@ -28,17 +28,7 @@ const registerSchema = z
   .object({
     firstName: z.string().min(2, "First name must be at least 2 characters"),
     lastName: z.string().min(2, "Last name must be at least 2 characters"),
-    email: z
-      .string()
-      .email("Please enter a valid email")
-      .refine(
-        (email) =>
-          email.endsWith(".edu") ||
-          email.endsWith(".edu.gh") ||
-          email.includes("st.ug.edu.gh") ||
-          email.includes("upsa.edu.gh"),
-        "Please use your university email",
-      ),
+    email: z.string().email("Please enter a valid email"),
     phoneNumber: z
       .string()
       .min(10, "Phone number must be at least 10 digits")
