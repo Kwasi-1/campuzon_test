0674189185da7425f3457ff92bc305934c769e09
Davisone <67480359+krush-ghod@users.noreply.github.com>
2026-04-23 14:50:24 -0400

    Minor Update

--- src/pages/seller/pages/dashboard/index.tsx
+++ src/pages/seller/pages/dashboard/index.tsx
@@ -209,7 +209,7 @@ export function SellerDashboardPage() {
 
   return (
     <SellerPageTemplate
-      title={`Welcome back, ${user.firstName}!`}
+      title={`Welcome back, ${user.store?.storeName}!`}
       description="Here's what's happening with your store today."
       headerActions={
         <div className="flex gap-2">
